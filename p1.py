662d1725084eae1e9c6a6307e4810eac479feb8c
Mohammad Jihad <Jihadu2006@gmail.com>
2024-07-21 17:08:06 -0400

    more

--- p1.py
+++ p1.py
@@ -43,7 +43,9 @@ output = np.dot(weights, inputs) + bias
 
 import numpy as np
 
-inputs = [1,2,3,2.5]
+inputs = [[1,2,3,2.5],
+          [2.0,5,-1,2],
+          [-1.5,2.7,3.3,-.8]]
 
 weights = [[.2,.8,-.5,1],
            [.5,-.91,.26,-.5],
@@ -51,7 +53,19 @@ weights = [[.2,.8,-.5,1],
 
 bias = [2,3,.5]
 
+#convert to numpy array
 
-output = np.dot(weights, inputs) + bias
-print(output)
+#transpose matrix
 
+
+weights2 = [[.1,-.14,.5],
+           [-.5,.12,-.33],
+           [-.44,.73,-.13]]
+
+bias2 = [-1,2,-.5]
+
+layer1_output = np.dot(inputs,np.array(weights).T) + bias
+
+layer2_output = np.dot(layer1_output,np.array(weights2).T) + bias2
+
+print(layer2_output)
